2fed18e05def8d9f2440637763fb0a6b94693183
ivan <prog@gmail.com>
2020-01-27 14:35:49 +0300

    first_commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="css/style.css" rel="stylesheet" type="text/css" >
+	
+	<title>Дни рождения</title>
+</head>
+<body>
+<header>
+
+</header>
+
+<div class="content">
+	<div class="main_wrapper">
+		<div class="banners_container">
+			<div class="main_banner"></div>
+			<div class="middle_banners">
+				<div class="middle_banners__square"></div>
+				<div class="middle_banners__square"></div>
+				<div class="middle_banners__youtobe"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<footer>
+
+</footer>
+
+</body>
+</html>
