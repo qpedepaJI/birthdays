e9b74105deb8d4d0bcf010ed6ab9d50f0c862a71
ivan <prog@gmail.com>
2020-01-29 16:35:21 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -3,20 +3,20 @@ $(document).ready(function() {
     let btn = $('.additional_btn').text();
     console.log(btn);
 
-    var mySwiper = new Swiper ('.swiper-container', {
-        // Optional parameters
-        loop: true,
-        speed: 700,
-        setWrapperSize:true,
-        centeredSlides:true,
-        slidesPerView:1,
-        dots:false,
-
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-
-    })
+    // var mySwiper = new Swiper ('.swiper-container', {
+    //     // Optional parameters
+    //     loop: true,
+    //     speed: 700,
+    //     setWrapperSize:true,
+    //     centeredSlides:true,
+    //     slidesPerView:1,
+    //     dots:false,
+    //
+    //     // Navigation arrows
+    //     navigation: {
+    //         nextEl: '.swiper-button-next',
+    //         prevEl: '.swiper-button-prev',
+    //     },
+    //
+    // })
 });
