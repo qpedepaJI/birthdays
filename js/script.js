bfbf2ea4b4eba08eda025c28d56f723eab1bdf29
ivan <prog@gmail.com>
2020-02-03 10:55:29 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -18,6 +18,23 @@ $(document).ready(function() {
             prevEl: '.swiper-button-prev',
         },
 
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+
+            },
+            // when window width is >= 480px
+            480: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            // when window width is >= 640px
+            640: {
+                slidesPerView: 4,
+                spaceBetween: 40
+            }
+        }
+
     })
 
 });
