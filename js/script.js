22f6f00fb26bdb73891b269ae05e206de193ee50
ivan <prog@gmail.com>
2020-02-03 11:21:07 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -12,29 +12,9 @@ $(document).ready(function() {
         slidesPerView:1,
         dots:false,
 
-        // Navigation arrows
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-
-        breakpoints: {
-            // when window width is >= 320px
-            320: {
-
-            },
-            // when window width is >= 480px
-            480: {
-                slidesPerView: 3,
-                spaceBetween: 30
-            },
-            // when window width is >= 640px
-            640: {
-                slidesPerView: 4,
-                spaceBetween: 40
-            }
-        }
-
     })
-
 });
